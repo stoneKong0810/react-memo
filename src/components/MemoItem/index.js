efce60a40e2dd14c48398aaebd89c700bc7f95be
stoneKong0810 <stonekong2115@gmail.com>
2025-03-12 20:08:08 +0900

    Feat: 메모 삭제 기능 추가

---
+++ src/components/MemoItem/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+function MemoItem({ children: title, onClickItem, onClickDelete, isSelected }) {
+  return (
+    <div
+      className={'MemoItem' + (isSelected ? ' selected' : '')}
+      onClick={onClickItem}
+    >
+      {title}
+      <button className={'MemoItem__del-btn'} onClick={onClickDelete}>
+        X
+      </button>
+    </div>
+  );
+}
+
+export default MemoItem;
